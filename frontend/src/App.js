a412ccd464dd2da6ad7d7ef96f3d7559cd904c9a
Vitor Costa <vitorcostinha@hotmail.com>
2020-09-07 18:17:25 -0300

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default class App extends Component {
-  render() {
-    return <h1>teste</h1>
-  }
+
+export default function App () {
+
+  const [allCanecas, setAllCanecas] = useState([]);
+  const [allCamisas, setAllCamisas] = useState([]);
+  const [allSqueezes, setAllSqueezes] = useState([]);
+  const [allAdesivos, setAllAdesivos] = useState([]);
+
+
+
+  useEffect(() => {
+
+    const getItens = async () => {
+
+      const res = await fetch('http://localhost:3003/itens');
+      const json = await res.json()
+
+      const todosItens = json;
+
+      const todasCanecas = todosItens.canecas.map(({id, nome, descricao, valor, linkMl, foto }) => {
+          return { id, nome, descricao, valor,linkMl,foto };});
+
+      const todosSqueezes = todosItens.squeezes.map(({id, nome, descricao, valor, linkMl, foto }) => {
+          return { id, nome, descricao, valor,linkMl,foto };});
+
+      const todasCamisas = todosItens.camisas.map(({id, nome, descricao, valor, linkMl, foto }) => {
+          return { id, nome, descricao, valor,linkMl,foto };});
+
+      const todosAdesivos = todosItens.adesivos.map(({id, nome, descricao, valor, linkMl, foto }) => {
+          return { id, nome, descricao, valor,linkMl,foto };});
+
+
+      setAllCanecas(todasCanecas);
+      console.log(todasCanecas);
+
+      setAllSqueezes(todosSqueezes)
+      console.log(todosSqueezes);
+
+      setAllCamisas(todasCamisas)
+      console.log(todasCamisas);
+
+      setAllAdesivos(todosAdesivos)
+      console.log(todosAdesivos);
+    }
+
+    //executando a função acima
+    getItens();
+
+  }, [])
+
+
+
+
+
+
+    return (
+      <div>
+        testetetetete
+      </div>
+    )
+    
 }
